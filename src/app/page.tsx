3551de8eedb74e2c64b824998b2964f7fc289aa5
Md. Sadiqur Rahman <115367918+Sadiqur057@users.noreply.github.com>
2025-11-18 16:08:16 +0600

    fix: correct import paths and clean up unused components in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
-'use client';
+"use client";
 
-import React from 'react';
-import HeroSection from '@/components/sections/HeroSection';
-import FeaturesSection from '@/components/sections/FeaturesSection';
-import ProcessSection from '@/components/sections/ProcessSection';
-import PricingSection from '@/components/sections/PricingSection';
-import TestimonialsSection from '@/components/sections/TestimonialsSection';
-import FAQSection from '@/components/sections/FAQSection';
-import ProjectsSection from '@/components/sections/ProjectsSection';
-import ClientFeedback from '@/components/sections/PricingSection';
-import PricingSection6 from '@/components/sections/TestimonialsSection';
-import WhatWeDoSection from '@/components/sections/WhatWeDoSection';
-import PlatformComparisonSection from '@/components/sections/PlatformComparisonSection';
-import MarqueeSection from '@/components/sections/MarqueeSection';
-import FlexibleElementsSection from '@/components/sections/FlexibleElementsSection';
-import AgencyProblemsSection from '@/components/sections/AgencyProblemsSection';
-import TechStackSection from '@/components/sections/TechStackSection';
-import Carv from '@/components/sections/Carv';
-import Feature3 from '@/components/sections/StackSection';
-import RotatingMarqueeSection from '@/components/sections/RotatingMarqueeSection';
+import React from "react";
+import HeroSection from "@/components/sections/HeroSection";
+import FeaturesSection from "@/components/sections/FeaturesSection";
+import ProcessSection from "@/components/sections/ProcessSection";
+import TestimonialsSection from "@/components/sections/PricingSection";
+import FAQSection from "@/components/sections/FAQSection";
+import ProjectsSection from "@/components/sections/ProjectsSection";
+import ClientFeedback from "@/components/sections/ClientFeedbackSection";
+import WhatWeDoSection from "@/components/sections/WhatWeDoSection";
+import MarqueeSection from "@/components/sections/MarqueeSection";
+import FlexibleElementsSection from "@/components/sections/FlexibleElementsSection";
+import AgencyProblemsSection from "@/components/sections/AgencyProblemsSection";
+import TechStackSection from "@/components/sections/TechStackSection";
+import Carv from "@/components/sections/Carv";
+import Feature3 from "@/components/sections/StackSection";
+import RotatingMarqueeSection from "@/components/sections/RotatingMarqueeSection";
+import PricingSection from "@/components/sections/PricingSection";
+import PlatformComparisonSection from "@/components/sections/PlatformComparisonSection";
+import PlatformComparisonSectionOld from "@/components/sections/PlatformComparisonSection-old";
 
 export default function Home() {
   return (
@@ -32,12 +32,13 @@ export default function Home() {
       <ProcessSection />
       <WhatWeDoSection />
       <PlatformComparisonSection />
+      {/* <PlatformComparisonSectionOld /> */}
       {/* <MarqueeSection /> */}
       <Carv />
       {/* <TechStackSection /> */}
       <Feature3 />
       <ClientFeedback />
-      <PricingSection6 />
+      <PricingSection />
       <FAQSection />
     </main>
   );
